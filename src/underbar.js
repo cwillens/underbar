9be237dbc9e7a7aef8f66471dc3471d3bfe3d6f8
Charlotte Willens <charwillens@gmail.com>
2016-08-18 15:31:58 -0700

    modified functions up through filter

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length-1] : array.slice(array.length-Math.min(n, array.length));
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +47,18 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i=0; i<collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    }
+
+    else {
+      var keys=Object.keys(collection);
+      for (var key of keys) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,6 +80,11 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result=[];
+    for (var i=0; i<collection.length; i++) {
+      if (test(collection[i])===true) result.push(collection[i]);
+    }
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
